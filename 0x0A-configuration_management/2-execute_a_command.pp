b84a6474833490545b4293a1420ae533768c8461
MichaelAuditore <michael_v613@hotmail.com>
2020-03-23 15:48:43 -0500

    [Fix] path attribute for pkill command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # Using Puppet, create a manifest that kills a process named killmenow.
 exec { 'pkill':
-  command  => 'pkill -f killmenow',
-  provider => 'shell',
-  returns  => [0,1],
+  command => 'pkill -f killmenow',
+  path    => 'usr/bin',
 }
