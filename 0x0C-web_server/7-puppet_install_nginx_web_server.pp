e330e084f3d9a2c8360f439a127542cc24790d19
MichaelAuditore <michael_v613@hotmail.com>
2020-03-25 11:10:24 -0500

    [Fix] Using multiples exec for execution

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,34 @@
-# Install Nginx web server with a puppet script
-exec { 'nginx':
-  provider => 'shell',
-  command  => 'sudo apt-get update -y;sudo apt-get install nginx -y;echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html;sudo sed -i "s/server_name_;/server_name _;\n\trewrite ^\/redirect_me https:\/\/michaelauditore.github.io permanent;/" /etc/nginx/sites-available/default;sudo service nginx start',
+# Manifest that configures NGINX
+exec { 'cmd_0':
+  path    => '/usr/bin:/bin',
+  command => 'sudo sudo apt-get update -y',
+  returns => [0,1]
+}
+
+exec { 'cmd_1':
+  require => Exec['cmd_0'],
+  path    => '/usr/bin:/bin',
+  command => 'sudo apt-get install nginx -y',
+  returns => [0,1]
+}
+
+exec { 'cmd_2':
+  require => Exec['cmd_1'],
+  path    => '/usr/bin:/bin',
+  command => 'echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html',
+  returns => [0,1]
+}
+
+exec { 'cmd_3':
+  require => Exec['cmd_2'],
+  path    => '/usr/bin:/bin',
+  command => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me michaelauditore.github.io permanent;/" /etc/nginx/sites-enabled/default',
+  returns => [0,1]
+}
+
+exec { 'cmd_4':
+  require => Exec['cmd_3'],
+  path    => '/usr/bin:/bin:/usr/sbin',
+  command => 'sudo service nginx start',
+  returns => [0,1]
 }
