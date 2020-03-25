77cca9eb0329d6623f6b8a45e8910f8ff08aa7e8
MichaelAuditore <michael_v613@hotmail.com>
2020-03-24 21:38:16 -0500

    [New file]puppet script to configure by first time nginx server and deploy it

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,7 @@
+# Install Nginx web server with a puppet script
+exec { 'nginx':
+  provider => 'shell',
+  command  => 'sudo apt-get update -y;sudo apt-get install nginx -y;echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html;sudo sed -i "s/server_name_;/server_name _;\n\trewrite ^\/redirect_me https:\/\/michaelauditore.github.io permanent;/" /etc/nginx/sites-available/default;
+  sudo service nginx start',
+  returns  =>  [0,1]
+}
