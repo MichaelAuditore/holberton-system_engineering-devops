553ac2c70665fe76dde7da598596079017b4c2a8
MichaelAuditore <michael_v613@hotmail.com>
2020-03-25 10:41:33 -0500

    [Fix] Command

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,5 @@
 # Install Nginx web server with a puppet script
 exec { 'nginx':
   provider => 'shell',
-  command  => 'sudo apt-get update -y;sudo apt-get install nginx -y;echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html;sudo sed -i "s/server_name_;/server_name _;\n\trewrite ^\/redirect_me https:\/\/michaelauditore.github.io permanent;/" /etc/nginx/sites-available/default;
-  sudo service nginx start',
-  returns  =>  [0,1]
+  command  => 'sudo apt-get update -y;sudo apt-get install nginx -y;echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html;sudo sed -i "s/server_name_;/server_name _;\n\trewrite ^\/redirect_me https:\/\/michaelauditore.github.io permanent;/" /etc/nginx/sites-available/default;sudo service nginx start',
 }
