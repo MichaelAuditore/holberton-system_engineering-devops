ad49ea30a1b0942d31fc91ba982a6a2ab571a0d6
MichaelAuditore <michael_v613@hotmail.com>
2020-05-04 11:30:13 -0500

    [New script] get some Employee data from an API and save into a csv with a specific format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Script to do a GET request to get Employee
+Data from an API and converts to CSV"""
+import requests
+import sys
+import csv
+
+url = 'https://jsonplaceholder.typicode.com'
+username = requests.get(url + '/users/' + sys.argv[1]).json().get('username')
+todos = requests.get(url + '/todos').json()
+with open(sys.argv[1] + '.csv', "w") as f:
+    for todo in todos:
+        rtask = str(todo.get('completed'))
+        dtask = todo.get('title')
+        if (todo.get('userId') == int(sys.argv[1])):
+            writer = csv.writer(f, quotechar='"', quoting=csv.QUOTE_ALL)
+            writer.writerow([sys.argv[1], username, rtask, dtask])
+f.closed
