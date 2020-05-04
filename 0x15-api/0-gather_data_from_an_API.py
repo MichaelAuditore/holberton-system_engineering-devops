072965c96eaf12205536983fd52899e13c177136
MichaelAuditore <michael_v613@hotmail.com>
2020-05-04 16:43:17 -0500

    [Fix] Basic solution Output fixed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,18 +9,19 @@ if __name__ == "__main__":
     except:
         exit()
     url = 'https://jsonplaceholder.typicode.com'
-    name = requests.get(url + '/users/' + sys.argv[1]).json().get('name')
-    todos = requests.get(url + '/todos').json()
+    try:
+        name = requests.get(url + '/users/' + sys.argv[1]).json().get('name')
+        todos = requests.get(url + '/todos').json()
+    except ValueError:
+        print("Not a valid JSON")
     ntasks = 0
     rtasks = 0
-    stasks = "\n\t"
     for todo in todos:
         if todo.get('userId') == int(sys.argv[1]):
             ntasks += 1
         if (todo.get('completed') and todo.get('userId') == int(sys.argv[1])):
             rtasks += 1
-            stasks += todo.get('title') + '\n\t'
-    print('Employee {} is done with tasks({}/{}):{}'.format(name,
-                                                            rtasks,
-                                                            ntasks,
-                                                            stasks[:-2]))
+    print('Employee {} is done with tasks({}/{}):'.format(name, rtasks, ntasks))
+    for todo in todos:
+        if (todo.get('completed') and todo.get('userId') == int(sys.argv[1])):
+            print('\t{}'.format(todo.get('title')))
