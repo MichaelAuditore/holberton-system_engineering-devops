54148d0526ccb4b84481e9133da8c9871d03493b
MichaelAuditore <michael_v613@hotmail.com>
2020-05-04 11:29:49 -0500

    [New script] Script to get some data from an API of an Employee

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Script to do a GET request to get Employee Data from an API"""
+import requests
+import sys
+
+url = 'https://jsonplaceholder.typicode.com'
+name = requests.get(url + '/users/' + sys.argv[1]).json().get('name')
+todos = requests.get(url + '/todos').json()
+ntasks = 0
+rtasks = 0
+stasks = "\n\t"
+for todo in todos:
+    if todo.get('userId') == int(sys.argv[1]):
+        ntasks += 1
+    if (todo.get('completed') and todo.get('userId') == int(sys.argv[1])):
+        rtasks += 1
+        stasks += todo.get('title') + '\n\t'
+print('Employee {} is done with tasks({}/{}):{}'.format(name,
+                                                        rtasks,
+                                                        ntasks,
+                                                        stasks[:-2]))
