fb329375995e95fdaf82207b49c67329fc8e175b
MichaelAuditore <michael_v613@hotmail.com>
2020-05-04 16:12:07 -0500

    [Fix] Should not execute when imported

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,19 +3,20 @@
 import requests
 import sys
 
-url = 'https://jsonplaceholder.typicode.com'
-name = requests.get(url + '/users/' + sys.argv[1]).json().get('name')
-todos = requests.get(url + '/todos').json()
-ntasks = 0
-rtasks = 0
-stasks = "\n\t"
-for todo in todos:
-    if todo.get('userId') == int(sys.argv[1]):
-        ntasks += 1
-    if (todo.get('completed') and todo.get('userId') == int(sys.argv[1])):
-        rtasks += 1
-        stasks += todo.get('title') + '\n\t'
-print('Employee {} is done with tasks({}/{}):{}'.format(name,
-                                                        rtasks,
-                                                        ntasks,
-                                                        stasks[:-2]))
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com'
+    name = requests.get(url + '/users/' + sys.argv[1]).json().get('name')
+    todos = requests.get(url + '/todos').json()
+    ntasks = 0
+    rtasks = 0
+    stasks = "\n\t"
+    for todo in todos:
+        if todo.get('userId') == int(sys.argv[1]):
+            ntasks += 1
+        if (todo.get('completed') and todo.get('userId') == int(sys.argv[1])):
+            rtasks += 1
+            stasks += todo.get('title') + '\n\t'
+    print('Employee {} is done with tasks({}/{}):{}'.format(name,
+                                                            rtasks,
+                                                            ntasks,
+                                                            stasks[:-2]))
