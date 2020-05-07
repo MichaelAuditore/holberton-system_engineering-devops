cb8aab860a3ed7e9cc01538b1b3584697f7c4231
MichaelAuditore <michael_v613@hotmail.com>
2020-05-07 18:11:01 -0500

    [New function] returns a list of all hot posts of a subreddit or None if a fake subreddit is given

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Return a list for all hot post in a given subreddit in REDDIT's API,
+invalid subreddit should return an Empty list"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None, time=0):
+    URL = 'https://www.reddit.com/r/' + subreddit + '/hot.json'
+    header = {'user-agent': 'miguel/0.0.1'}
+    req = requests.get(URL, headers=header, allow_redirects=False,
+                           params={'after': str(after)})
+    if req.status_code == 200:
+        data = req.json()
+        hot_list += [item for item in data['data']['children']]
+        after = data['data']['after']
+        if not after:
+            return hot_list
+        else:
+            time += 1
+            result = recurse(subreddit, hot_list, after, time)
+    else:
+        return (None)
+
+    return hot_list
