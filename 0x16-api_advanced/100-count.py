f3761ba264654bdde2e388f46137c204f1baf221
MichaelAuditore <michael_v613@hotmail.com>
2020-05-07 21:07:02 -0500

    [Fix] condition statement

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -21,8 +21,8 @@ def count_words(subreddit, word_list, after=None, dic=None, item=0):
         after = data['data']['after']
         for title in titles:
             title = title.split(' ')
-            for sb in title:
-                for word in word_list:
+            for word in word_list:
+                for sb in title:
                     if sb.lower() == word.lower():
                         dic[word] += 1
         if after:
