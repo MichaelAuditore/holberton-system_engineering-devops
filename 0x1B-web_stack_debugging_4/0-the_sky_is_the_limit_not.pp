8e37582ad8b9087ea78af2816292ce542c5569f8
MichaelAuditore <michael_v613@hotmail.com>
2020-05-25 17:17:24 -0500

    [New puppet to solve the problem]

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+exec { 'max-requests':
+  command  => 'sed -i "s/15/3000" /etc/default/nginx',
+  provider => 'shell',
+  before   => Exec['restart-nginx'].
+}
+
+exec { 'restart-nginx':
+  command  => 'sudo service nginx restart',
+  provider => 'shell',
+}
