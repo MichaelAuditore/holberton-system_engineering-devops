a00ed7bb3500440cda404a2ce96aef48931a3835
Miguel Angel Parada Cañon <michael_v613@hotmail.com>
2020-05-28 09:59:24 -0500

    Fix
    Rewritten again

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,12 @@
 # Manifiest to change the maximum of requests given to server
-exec { 'max-requests':
+
+exec { 'change':
   command  => 'sudo sed -i "s/15/3000/" /etc/default/nginx',
-  provider => 'shell',
-  before   => Exec['restart-nginx'].
+  provider => shell,
+  before   => Exec['restart'],
 }
 
-exec { 'restart-nginx':
+exec { 'restart':
   command  => 'sudo service nginx restart',
-  provider => 'shell',
+  provider => shell,
 }
